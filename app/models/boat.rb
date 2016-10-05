4a036bd24c1e4ee28eca727c65d7f62425f84f99
sami4thewin <samiomarmohammed@gmail.com>
2016-10-05 13:29:50 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,39 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.limit(5)
+  end
+
+  def self.dinghy
+    Boat.where("length < ?", 20)
+  end
+
+  def self.ship
+    Boat.where("length > ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    # binding.pry
+    Boat.order(:name).reverse_order.limit(3)
+  end
+
+  def self.without_a_captain
+    Boat.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    # binding.pry
+    Boat.joins(:classifications).where("classifications.name" => "Sailboat")
+  end
+
+  def self.with_three_classifications
+    # binding.pry
+    # a = Boat.joins(:boat_classifications).group(:boat_id).count
+    # b = a.select {|k,v| v == 3}
+    # Boat.where(id: b.keys)
+    Boat.joins(:boat_classifications).group(:boat_id).having("count(classification_id) = ?", 3)
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,33 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+    Captain.joins(:classifications).where("classifications.name = ?", "Catamaran")
+  end
+
+  def self.sailors
+    Captain.joins(:classifications).where("classifications.name = ?", "Sailboat").uniq
+  end
+
+  #seamen motorboaters and sailer, make own method MOTORBOATERS to find motorboaters
+  #combining the two SQL queries together
+
+  def self.motorboaters
+    Captain.joins(:classifications).where("classifications.name = ?", "Motorboat").uniq
+  end
+
+  def self.talented_seamen
+    # binding.pry
+    a = Captain.motorboaters & Captain.sailors
+    Captain.where(id: a.map {|b| b.id  })
+  end
+
+  def self.non_sailors
+    # binding.pry
+    # Captain.joins(:classifications).where("classifications.name = ?", "Sailboat").uniq
+    a = Captain.sailors
+    Captain.where.not(id: a.map {|b| b.id  })
+  end
+
 end
